db697833b850c2dc299e6304c039fcc8b8870703
omgimanerd <alvin@omgimanerd.tech>
2018-01-28 04:32:02 -0500

    Cleanup and whoohoo

--- server.js
+++ server.js
@@ -3,16 +3,14 @@
  * @author alvin@omgimanerd.tech (Alvin Lin)
  */
 
-const PORT = 5000;
+const PORT = 5000
 
 // Dependencies.
 const bodyParser = require('body-parser')
 const express = require('express')
-const fs = require('fs')
 const http = require('http')
 const path = require('path')
 const youtubeAudioStream = require('youtube-audio-stream')
-const ytdl = require('ytdl-core')
 
 const app = express()
 const server = http.Server(app)
@@ -22,7 +20,6 @@ app.set('port', PORT)
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')))
 app.use('/client', express.static(path.join(__dirname, '/client')))
-app.use('/videos', express.static(path.join(__dirname, '/videos')))
 
 app.get('/', (request, response) => {
   response.sendFile(path.join(__dirname, 'index.html'))
@@ -36,30 +33,6 @@ app.get('/stream/:videoId', (request, response) => {
   } catch (exception) {
     console.error(exception)
     response.status(500).send(exception)
-  }
-})
-
-app.post('/spatialize', (request, response) => {
-  const videoUrl = request.body.url
-  if (!ytdl.validateURL(videoUrl)) {
-    response.send({ success: false })
-    return
-  }
-  const videoId = ytdl.getVideoID(videoUrl)
-  const videoWriteStream = fs.createWriteStream(
-    path.join(__dirname, 'videos', `${videoId}.mp4`))
-  try {
-    ytdl(videoUrl, { filter: format => format.container == 'mp4' })
-      .pipe(videoWriteStream)
-    videoWriteStream.on('close', () => {
-      response.send({
-        success: true,
-        id: videoId
-      })
-    })
-  } catch (exception) {
-    console.error(exception)
-    response.send({ success: false })
   }
 })
 
--- videos/README.md
+++
@@ -1,1 +0,0 @@
-Storage for videos
