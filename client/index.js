7bb6de9bce00ed7d660a35ff7a58204eaa9014d1
omgimanerd <alvin@omgimanerd.tech>
2018-01-28 01:01:59 -0500

    Some base visualization stuff

--- client/index.js
+++ client/index.js
@@ -3,41 +3,59 @@
  * @author omgimanerd (alvin@omgimanerd.tech)
  * @author Searnsy
  */
+/* eslint-disable require-jsdoc */
+/* globals $, ResonanceAudio */
 
-const UPDATE_RATE = 10
+const UPDATE_RATE = 1
 
 $(document).ready(() => {
   const audioContext = new AudioContext()
   const resonanceAudio = new ResonanceAudio(audioContext)
   resonanceAudio.output.connect(audioContext.destination)
 
+  let processing = false
+  $('.button-loading').hide()
+  $('.button-idle').show()
+
   $('.video-input-form').submit(() => {
-    const videoUrl = $('.video-url-input').val()
-    $.post('/spatialize', { url: videoUrl }).done(data => {
-      if (!data.success) {
-        alert('There was an error decoding this audio file! Fuck!')
-        return
-      }
-      const id = data.id
-      const positionVectors = data.positionVectors
+    if (!processing) {
+      const videoUrl = $('.video-url-input').val()
+      $('.video-url-input').prop('disabled', 'disabled')
+      $('.button-loading').show()
+      $('.button-idle').hide()
+      processing = true
 
-      const audioElement = document.createElement('audio')
-      audioElement.src = `/videos/${id}.mp4`
-      const audioElementSource =
-        audioContext.createMediaElementSource(audioElement)
-      const source = resonanceAudio.createSource()
-      audioElementSource.connect(source.input)
-
-      const startTimestamp = Date.now()
-      audioElement.play()
-      const audioId = setInterval(() => {
-        const deltaTime = Date.now() - startTimestamp
-        if (deltaTime > positionVectors.length || audioElement.ended) {
-          clearInterval(audioId)
+      $.post('/spatialize', { url: videoUrl }).done(data => {
+        $('.video-url-input').removeAttr('disabled')
+        $('.button-loading').hide()
+        $('.button-idle').show()
+        processing = false
+        if (!data.success) {
+          alert('There was an error decoding this audio file! Fuck!')
+          return
         }
-        source.setPosition(...positionVectors[deltaTime])
-      }, UPDATE_RATE)
-    })
+        const id = data.id
+        const positionVectors = data.positionVectors
+
+        const audioElement = document.createElement('audio')
+        audioElement.src = `/videos/${id}.mp4`
+        const audioElementSource =
+          audioContext.createMediaElementSource(audioElement)
+        const source = resonanceAudio.createSource()
+        audioElementSource.connect(source.input)
+
+        const startTimestamp = Date.now()
+        audioElement.play()
+        const audioId = setInterval(() => {
+          const deltaTime = Date.now() - startTimestamp
+          if (deltaTime > positionVectors.length || audioElement.ended) {
+            clearInterval(audioId)
+            return
+          }
+          source.setPosition(...positionVectors[deltaTime])
+        }, UPDATE_RATE)
+      })
+    }
     return false
   })
 })
---
+++ client/visualizer.js
@@ -0,0 +1,61 @@
+/**
+ * Client side script.
+ * @author omgimanerd (alvin@omgimanerd.tech)
+ * @author Searnsy
+ */
+/* eslint-disable require-jsdoc */
+/* globals $ */
+
+const TAU = 6.28
+
+// eslint-disable-next-line no-unused-vars
+const visualize = amplitudes => {
+  const canvasElement = document.getElementById('visualization-canvas')
+  const canvasContext = canvasElement.getContext('2d')
+
+  const width = canvasElement.width = $(document).width()
+  const height = canvasElement.height = $(document).height()
+
+  const cx = width / 2
+  const cy = height / 2.4
+  const startTimestamp = Date.now()
+  let l = 0
+  canvasContext.save()
+  canvasContext.translate(cx, cy)
+  const visualizationId = setInterval(() => {
+    // const deltaTime = Date.now() - startTimestamp
+    if (l >= amplitudes.length) {
+      canvasContext.restore()
+      canvasContext.clearRect(0, 0, width, height)
+      clearInterval(visualizationId)
+      return
+    }
+    canvasContext.clearRect(-width, -height, width * 2, height * 2)
+    canvasContext.fillStyle = 'red'
+    const circleRadius = (amplitudes[l].reduce((a, b) => a + b) / 40)
+    console.log(circleRadius)
+    canvasContext.arc(0, 0, circleRadius, 0, TAU)
+    canvasContext.fill()
+
+    canvasContext.save()
+    for (let i = 0; i < 40; ++i) {
+      canvasContext.fillRect(-30, 0, amplitudes[l][i] + circleRadius, 30)
+      canvasContext.rotate(TAU / 40)
+    }
+    canvasContext.restore()
+    ++l
+  }, 100)
+}
+
+$(document).ready(() => {
+  const data = []
+  for (let i = 0; i < 100; ++i) {
+    const a = []
+    for (let j = 0; j < 40; ++j) {
+      a.push(Math.random() * 300)
+    }
+    data.push(a)
+  }
+  console.log(data)
+  visualize(data)
+})
